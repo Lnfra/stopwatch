5dd60dca28828b040f1ac6cbb90361d491df897c
Angeline Tan <atan@gmail.com>
2016-02-22 23:53:51 +0800

    Fixed error where multiple timers are created if start button is clicked many times. Stopwatch ver 1.1

--- timers.js
+++ timers.js
@@ -21,22 +21,29 @@ function init() {
 
     //Add listeners
     resetBtn.addEventListener('click', function(){
-        //Stop the timer
+        //Stop the timer and set its value back to 0
         clearInterval(timer.timerId);
+        timer.timerId = 0;
         //Clear the secs counter
         timer.seconds = 0;
         document.getElementById('timer').innerHTML = 'Stop Watch';
     });
 
     startBtn.addEventListener('click', function(){
-        //Call increment timer every 1 sec (1000 milisec)setInter
-        timer.timerId = setInterval(timer.updateTime.bind(timer), 1000);
+        //Only start the timer if it is not already started
+        if(timer.timerId == 0) {
+            //Call increment timer every 1 sec (1000 milisec)setInter
+            timer.timerId = setInterval(timer.updateTime.bind(timer), 1000);
+        }else{
+            console.log('Ignored click as timer already started');
+        }
 
     });
 
     pauseBtn.addEventListener('click', function(){
-        //Stop the secs counter
         clearInterval(timer.timerId);
+        //If timer is stopped set the timerId to 0 so that it can be started again.
+        timer.timerId = 0;
     });
 
 }
